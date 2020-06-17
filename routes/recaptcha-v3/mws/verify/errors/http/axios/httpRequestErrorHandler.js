78465379fed1b67aa8c67c05d9214d686c7b10a0
Den Pol <selection4test@gmail.com>
2020-06-18 01:23:45 +0300

    master. fix

--- routes/recaptcha-v3/mws/verify/errors/http/axios/httpRequestErrorHandler.js
+++ routes/recaptcha-v3/mws/verify/errors/http/axios/httpRequestErrorHandler.js
@@ -2,10 +2,10 @@ const { HttpRequestError } = require('../HttpRequestError');
 
 const httpRequestErrorHandler = (obj) => {
   console.log(obj)
-  if (obj.request.status === 200) {
-    return obj.data
+  if (obj.response.status === 200) {
+    return obj
   } else {
-    throw new HttpRequestError(obj.request.status, obj.request.statusText)
+    throw new HttpRequestError(obj.response.status, obj.response.statusText)
   }
 }
 
