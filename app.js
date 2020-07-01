64299f3b2175023fc1fefd9cceb7a205d9863445
Den Pol <selection4test@gmail.com>
2020-07-02 00:14:19 +0300

    master. fix

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-require("dotenv").config();
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+require('dotenv').config()
 
-console.log(process.env.RECAPTCHAV3_VERIFY_URL);
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const urlMetadataRouter = require('./routes/url-metadata')
+const reCAPTCHAV3Router = require('./routes/recaptcha-v3')
+const swaggerRouter = require('./routes/swagger')
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var urlMetadataRouter = require("./routes/url-metadata");
-var reCAPTCHAV3Router = require("./routes/recaptcha-v3");
-var swaggerRouter = require("./routes/swagger");
+const app = express()
 
-var app = express();
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/url-metadata', urlMetadataRouter)
+app.use('/recaptcha-v3', reCAPTCHAV3Router)
+app.use('/swagger', swaggerRouter)
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/url-metadata", urlMetadataRouter);
-app.use("/recaptcha-v3", reCAPTCHAV3Router);
-app.use("/swagger", swaggerRouter);
-
-module.exports = app;
+module.exports = app
