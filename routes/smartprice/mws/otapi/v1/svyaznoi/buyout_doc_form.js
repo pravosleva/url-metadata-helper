54e668dceb5b5511b3e6c0ab4ea5787366075b58
Den <selection4test@gmail.com>
2021-03-12 15:46:24 +0300

    buyout_form validation added

--- routes/smartprice/mws/otapi/v1/svyaznoi/buyout_doc_form.js
+++ routes/smartprice/mws/otapi/v1/svyaznoi/buyout_doc_form.js
@@ -1,9 +1,29 @@
+/* eslint-disable no-restricted-syntax */
 const { getRandomInteger } = require('utils/getRandomInteger')
+
+const requiredFields = {
+  birth_date: true,
+  email: true,
+  first_name: true,
+  id_address: true,
+  id_city: true,
+  id_date_of_issue: true,
+  id_issued_by: true,
+  id_issuing_agency_code: true,
+  id_number: true,
+  id_serial_number: true,
+  id_type: true,
+  last_name: true,
+  patronymic: true,
+  phone: true,
+}
 
 const toClient = [
   {
     ok: false,
-    message: 'Test',
+    code: 'bad_request',
+    message: 'Неверные параметры запроса',
+    extra: null,
   },
   {
     ok: true,
@@ -30,11 +50,42 @@ const toClient = [
 */
 
 module.exports = async (req, res) => {
-  const toBeOrNotToBe = getRandomInteger(0, 1)
+  let toBeOrNotToBeIndex = 0
+  const internalErrorResult = {
+    ok: true,
+  }
+
+  if (Object.keys(req.body).length === 0) {
+    internalErrorResult.ok = false
+    internalErrorResult.code = 'bad_request'
+    internalErrorResult.message = 'Ни одного поля не найдено'
+  } else {
+    // noEmptyFields
+    for (const key in requiredFields) {
+      if (requiredFields[key] && !req.body[key]) {
+        // Обязательное поле из нот детектед
+        internalErrorResult.ok = false
+        internalErrorResult.code = 'bad_request'
+        if (!internalErrorResult.message) {
+          internalErrorResult.message = `Errored fields detected: ${key}`
+        } else {
+          internalErrorResult.message += `, ${key}`
+        }
+      }
+    }
+
+    // toBeOrNotToBeIndex = getRandomInteger(0, 1)
+    if (internalErrorResult.ok) {
+      toBeOrNotToBeIndex = 1
+    }
+  }
+
+  const result = toClient[toBeOrNotToBeIndex]
 
   setTimeout(() => {
     res.status(200).send({
-      ...toClient[toBeOrNotToBe],
+      ...result,
+      ...internalErrorResult,
       _originalBody: req.body,
     })
   }, 500)
